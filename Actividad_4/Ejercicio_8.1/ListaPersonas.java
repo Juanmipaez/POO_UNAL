06ff11b82c4c9d984b36ee52916bed2680f90013
Juanmipaez13 <85180095+Juanmipaez@users.noreply.github.com>
2024-08-20 11:05:34 -0500

    Add files via upload

---
+++ Actividad_4/Ejercicio_8.1/ListaPersonas.java
@@ -0,0 +1,24 @@
+package personas;
+
+import java.util.Vector;
+
+public class ListaPersonas {
+    Vector listaPersonas;
+
+    public ListaPersonas() {
+        listaPersonas = new Vector<>();
+    }
+
+    public void eliminarPersona(int i) {
+        listaPersonas.removeElementAt(i);
+    }
+
+    public void borrarLista() {
+        listaPersonas.removeAllElements();
+    }
+    
+    public void añadirPersona(Persona p){
+        listaPersonas.add(p);
+    }
+
+}
