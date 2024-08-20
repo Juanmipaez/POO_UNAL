38bb74ec9e507d57cbf184362be4422825ac6002
Juanmipaez13 <85180095+Juanmipaez@users.noreply.github.com>
2024-08-20 11:02:24 -0500

    Add files via upload

---
+++ Actividad_4/Ejercicio_4.7/Perro.java
@@ -0,0 +1,18 @@
+package Animales;
+
+public class Perro extends Canido {
+
+    public String getSonido(){
+        return "Ladrido";
+    }
+    public String getAlimentos(){
+        return "Carnívora";
+    }
+    public String getHabitat(){
+        return "Doméstico";
+    }
+    public String getNombreCientifico(){
+        return "Canis lupus familiaris";
+    }
+
+}
