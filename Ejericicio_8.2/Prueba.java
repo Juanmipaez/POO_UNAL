77a60de8b098efa28aef4529fce032a318f19aeb
Juanmipaez13 <85180095+Juanmipaez@users.noreply.github.com>
2024-09-09 15:38:48 -0500

    Add files via upload

---
+++ Ejericicio_8.2/Prueba.java
@@ -0,0 +1,9 @@
+package POO_UNAL.Taller_5;
+
+import javax.swing.SwingUtilities;
+
+public class Prueba {
+        public static void main(String[] args) {
+        SwingUtilities.invokeLater(NotasGUI::new);
+    }
+}
